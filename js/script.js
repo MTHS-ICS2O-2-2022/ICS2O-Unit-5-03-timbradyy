81af5bffbb3e805144b56d7ff08fc5e93354b970
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-05-19 16:38:56 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr.Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tim Brady
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict";
+
+/**
+ * This function calculates the movie rating
+ */
+function myButtonClicked() {
+  const userAge = document.getElementById("age").value;
+  document.getElementById("age").innerHTML = userAge;
+
+  if (userAge >= 17) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see any movie.";
+  } else if (userAge >= 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see a PG 13 rated movie.";
+  } else if (userAge >= 5) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to a PG or G rated movie.";
+  } else {
+    document.getElementById("movie-rating").innerHTML =
+      "You are too young for most movies.";
+  }
+}
